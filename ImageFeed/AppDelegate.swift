81bb0bd42ac3e86455af519a411a7ca67a359992
TimurOz <ozik1959@gmail.com>
2022-11-22 12:19:14 +0300

    no message

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Hello ImageFeed")
+        print("403")
+        print("402")
         return true
     }
 
